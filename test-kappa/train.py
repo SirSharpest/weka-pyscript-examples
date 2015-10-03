83928db6bfe8e4a66844d0fd65f82ba0bc8caa7f
Christopher Beckham <chrispy645@gmail.com>
2015-10-03 23:08:58 +1300

    Some changes and integrate regression into this script

--- test-kappa/train.py
+++ test-kappa/train.py
@@ -2,6 +2,7 @@ import lasagne
 import theano
 from theano import tensor as T
 from lasagne.objectives import categorical_crossentropy as x_ent
+from lasagne.objectives import squared_error
 from lasagne.regularization import l2
 from lasagne.regularization import regularize_network_params as reg
 import sys
@@ -34,7 +35,12 @@ def soft_kappa_loss(pred_vector, actual_vector):
 
 def get_hybrid_loss(a,b):
     def this_hybrid_loss(pred_vector, actual_vector):
-        return a*x_ent(pred_vector, actual_vector) + b*soft_kappa_loss(pred_vector, actual_vector)
+        if a == 1 and b == 0:
+            return x_ent(pred_vector, actual_vector)
+        elif a == 0 and b == 1:
+            return soft_kappa_loss(pred_vector, actual_vector)
+        else:
+            return a*x_ent(pred_vector, actual_vector) + b*soft_kappa_loss(pred_vector, actual_vector)
     return this_hybrid_loss
 
 def expectation(q):
@@ -76,33 +82,77 @@ def network():
     )
     return l_out
 
+"""
+Functions for doing the regression (squared error loss) experiments.
+"""
+
+def compare_to_logistic():
+    num_attributes = len( args["attributes"] ) - 1
+    l_in = lasagne.layers.InputLayer( shape=(None, num_attributes) )
+    l_out = lasagne.layers.DenseLayer(
+        l_in,
+        num_units=args["num_classes"],
+        nonlinearity=lasagne.nonlinearities.rectify,
+        W=lasagne.init.GlorotUniform()
+    )
+    return "Number of params for logistic(): %i" % lasagne.layers.count_params(l_out)
+
+def get_num_hidden_units(p, k):
+    x = float(p*k + k - 1) / (p + 2)
+    #return (p*x + x + x + 1)
+    return int(round(x))
+
+def squared_error_net_adaptive():
+    num_attributes = len( args["attributes"] ) - 1
+    l_in = lasagne.layers.InputLayer( shape=(None, num_attributes) )
+    l_hidden = lasagne.layers.DenseLayer(
+        l_in,
+        num_units=get_num_hidden_units(num_attributes, args["num_classes"]),
+        nonlinearity=lasagne.nonlinearities.sigmoid,
+        W=lasagne.init.GlorotUniform()
+    )
+    l_out = lasagne.layers.DenseLayer(
+        l_hidden,
+        num_units=1,
+        nonlinearity=lasagne.nonlinearities.linear,
+        W=lasagne.init.GlorotUniform()
+    )
+    l_flatten = lasagne.layers.FlattenLayer(l_out, outdim=1)
+    print "Number of params for squared_error_net_adaptive(): %i" % lasagne.layers.count_params(l_flatten)
+    print compare_to_logistic()
+    return l_flatten
+
 def prepare():
 
     X = T.fmatrix('X')
     y = T.ivector('y')
 
-    if "logistic" not in args:
-        output_layer = network()
+    assert not ("regression" in args and "logistic" in args)
+
+    if "regression" in args:
+        output_layer = squared_error_net_adaptive()
     else:
         output_layer = logistic()
 
     all_params = lasagne.layers.get_all_params(output_layer)
 
-    a = args["a"]
-    b = args["b"]
-    loss_fn = get_hybrid_loss(a,b)
-
-    prob_vector = lasagne.layers.get_output(output_layer, X)
-
-    loss = loss_fn(prob_vector, y).mean() + args["lambda"]*reg(output_layer, l2)
-    alt_loss = x_ent(prob_vector, y).mean() + args["lambda"]*reg(output_layer, l2)
-
-    pred = T.argmax( prob_vector, axis=1 )
-    accuracy = T.mean( T.eq(pred,y) )
+    if "regression" in args:
+        prob_vector = lasagne.layers.get_output(output_layer, X)
+        loss = squared_error(prob_vector, y).mean()
+        pred = T.maximum(0, T.minimum( T.round(prob_vector), args["num_classes"]-1 ) )
+        accuracy = T.mean( T.eq( pred, y ) )
+    else:
+        a = args["a"]
+        b = args["b"]
+        loss_fn = get_hybrid_loss(a,b)
+        prob_vector = lasagne.layers.get_output(output_layer, X)
+        loss = loss_fn(prob_vector, y).mean()
+        pred = T.argmax( prob_vector, axis=1 )
+        accuracy = T.mean( T.eq(pred,y) )
 
     return Container(
         { "X": X, "y": y, "output_layer": output_layer, "all_params": all_params,
-        "loss": loss, "pred": pred, "accuracy": accuracy, "alt_loss": alt_loss,
+        "loss": loss, "pred": pred, "accuracy": accuracy,
         "prob_vector": prob_vector
         }
     )
@@ -116,24 +166,16 @@ def train(arg):
     if "debug" in args and args["debug"]:
         debug = True
 
-    if not os.path.exists("/tmp/exp/"):
-        os.makedirs("/tmp/exp")
-    if "kappa" in args or "hybrid" in args:
-        g = open( "/tmp/exp/kappa.txt", "wb" )
-    else:
-        g = open( "/tmp/exp/normal.txt", "wb" )
-    g.write("loss,alt_loss\n")
-
-    alpha = args["alpha"]
+    alpha = theano.shared( args["alpha"] )
     momentum = 0.9
 
     if alpha != -1:
-        if "rmsprop" not in args:
-            updates = lasagne.updates.momentum(symbols.loss, symbols.all_params, alpha, momentum)
-        else:
+        if "rmsprop" in args:
             updates = lasagne.updates.rmsprop(symbols.loss, symbols.all_params, alpha)
-    else:
-        updates = lasagne.updates.adagrad(symbols.loss, symbols.all_params, 1.0)
+        elif "adagrad" in args:
+            updates = lasagne.updates.adagrad(symbols.loss, symbols.all_params, alpha)
+        else:
+            updates = lasagne.updates.nesterov_momentum(symbols.loss, symbols.all_params, alpha, momentum)
 
     iter_train = theano.function(
         [symbols.X, symbols.y],
@@ -145,17 +187,8 @@ def train(arg):
         [symbols.prob_vector, symbols.pred, symbols.loss, symbols.accuracy]
     )
 
-    iter_alt_loss = theano.function(
-        [symbols.X, symbols.y],
-        symbols.alt_loss
-    )
-
     args["X_train"] = np.asarray(args["X_train"], dtype="float32")
     args["y_train"] = np.asarray(args["y_train"].flatten(), dtype="int32")
-
-    idxs = [x for x in range(0, args["X_train"].shape[0])]
-    X_train = args["X_train"][idxs]
-    y_train = args["y_train"][idxs]
 
     if "batch_size" in args:
         bs = args["batch_size"]
@@ -163,49 +196,50 @@ def train(arg):
         bs = 128
 
     best_valid_accuracy = -1
+    last_loss = -100000
+    eps = 1e-6
+    tol_counter = 0
+    tolerance = 5
+    np.random.seed(SEED)
     for e in range(0, args["epochs"]):
 
-        np.random.seed(SEED)
-        np.random.shuffle(X_train)
-        np.random.seed(SEED)
-        np.random.shuffle(y_train)
+        # if we're doing sgd/msgd
+        if bs < X_train.shape[0]:
+            np.random.seed(SEED)
+            np.random.shuffle(X_train)
+            np.random.seed(SEED)
+            np.random.shuffle(y_train)
 
-        SEED += 1
-        np.random.seed(SEED)
+            SEED += 1
+            np.random.seed(SEED)
 
         sys.stderr.write("Epoch #%i:\n" % e)
         batch_train_losses = []
         batch_train_accuracies = []
-        batch_train_alt_losses = []
         for b in range(0, X_train.shape[0]):
             if b*bs >= X_train.shape[0]:
                 break
             #sys.stderr.write("  Batch #%i (%i-%i)\n" % ((b+1), (b*bs), ((b+1)*bs) ))
-            vectors, _, loss, acc = iter_train( X_train[b*bs : (b+1)*bs], y_train[b*bs : (b+1)*bs] )
-
+            vectors, pds, loss, acc = iter_train( X_train[b*bs : (b+1)*bs], y_train[b*bs : (b+1)*bs] )
+            if debug:
+                print vectors[0:10], pds[0:10]
             if "expectation" in args:
                 acc = sum( np.equal(expectations_rounded(vectors), y_train[b*bs : (b+1)*bs]) )
-
             batch_train_losses.append(loss)
             batch_train_accuracies.append(acc)
 
-            batch_train_alt_losses.append( iter_alt_loss(X_train[b*bs : (b+1)*bs], y_train[b*bs : (b+1)*bs]) )
-
-        batch_valid_losses = []
-        batch_valid_accuracies = []
-
-        #kappa = weighted_kappa(all_preds, y_valid.tolist())
-        kappa = 0
-
-        batch_valid_losses = [0]
-        valid_mean = 0
-        best_weights = lasagne.layers.get_all_param_values(symbols.output_layer)
-
-        sys.stderr.write( "  train_loss, train_accuracy, = %f, %f\n" % \
+        print( "  train_loss, train_accuracy, = %f, %f\n" % \
             (np.mean(batch_train_losses), np.mean(batch_train_accuracies)) )
-        g.write( str(np.mean(batch_train_losses)) + "," + str(np.mean(batch_train_alt_losses)) + "\n" )
 
-    g.close()
+        if abs(np.mean(batch_train_losses) - last_loss) < eps:
+            tol_counter += 1
+            if tol_counter == tolerance:
+                break
+        else:
+            last_loss = np.mean(batch_train_losses)
+            tol_counter = 0
+
+    best_weights = lasagne.layers.get_all_param_values(symbols.output_layer)
 
     return best_weights
 
@@ -214,7 +248,7 @@ def describe(arg, weights):
 
 def test(arg, weights):
 
-    global args, first_time_test, symbols, iter_test
+    global args, symbols, iter_test
     args = arg
 
     symbols = prepare()
@@ -245,14 +279,17 @@ def test(arg, weights):
 
 if __name__ == '__main__':
     x = ArffToArgs()
-    x.set_input("data/cpu_act.arff")
+    x.set_input("data/auto_price.arff")
     x.set_class_index("last")
     x.set_impute(True)
     x.set_binarize(True)
     x.set_standardize(True)
-    x.set_arguments("a=1;b=0;logistic=True;num_hidden_units=10;alpha=0.01;lambda=0;epochs=500;rmsprop=True")
+    x.set_arguments("a=1;b=0;logistic=True;alpha=0.1;rmsprop=True;epochs=10000;batch_size=1000000")
+    #x.set_arguments("a=1;b=0;regression=True;alpha=0.1;rmsprop=True;epochs=10000;batch_size=1000000")
+
+    #x.set_arguments("a=1;b=0;logistic=True;alpha=0.1;adagrad=True;epochs=50000;batch_size=1000000")
     args = x.get_args()
-    args["debug"] = True
+    args["debug"] = False
 
     args["X_test"] = np.asarray(args["X_train"], dtype="float32")
 
